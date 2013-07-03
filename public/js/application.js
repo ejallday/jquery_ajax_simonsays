8bd8e6a9596e544144b985e1801cb2d38294ec0e
Eric Allen <eric.justin.allen@gmail.com>
2013-07-03 10:40:40 -0500

    I think I'm done?

--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
-
+	$("#get_color").on('click', function(event){
+		event.preventDefault;
+		$.post('/color', function(data) {
+			$("li").eq(data.cell - 1).css("background-color", data.color);
+		});
+	});
 });
